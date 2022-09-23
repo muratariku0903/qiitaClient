effaa028bb989b54427b11cd1a829979d0032ed0
muratariku0903 <18ba179r@rikkyo.ac.jp>
2022-09-24 02:46:48 +0900

    add env file

---
+++ lib/presentation/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Qiita Client',
+      theme: ThemeData(primarySwatch: Colors.grey),
+      debugShowCheckedModeBanner: false,
+      home: const Text('hello'),
+    );
+  }
+}
